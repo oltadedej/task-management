b2c2d90032a7b14bbab189f1862bcff325330971
Olta Dedej <olta.dedej@corpay.com>
2026-01-12 15:08:18 +0100

    Updating tests

--- src/backend/TaskManagement/TaskManagement.Tests.Unit/Queries/GetTasksByStatusQueryHandlerTests.cs
+++ src/backend/TaskManagement/TaskManagement.Tests.Unit/Queries/GetTasksByStatusQueryHandlerTests.cs
@@ -1,14 +1,11 @@
 using AutoMapper;
 using FluentAssertions;
 using Moq;
-using TaskManagement.Domain.Entities;
-using TaskManagement.Domain.Enums;
 using TaskManagement.Domain.Exceptions;
 using TaskManagement.Infrastructure.Repositories;
 using TaskManagement.Service.Features.Tasks.Handlers;
 using TaskManagement.Service.Features.Tasks.Queries;
 using TaskManagement.Service.Mappings;
-using Xunit;
 using ManagementTask = TaskManagement.Domain.Entities.ManagementTask;
 using TaskStatus = TaskManagement.Domain.Enums.TaskStatus;
 
@@ -52,7 +49,7 @@ public class GetTasksByStatusQueryHandlerTests
             }
         };
 
-        var query = new GetTasksByStatusQuery { Status = "Completed" };
+        var query = new GetTasksByStatusQuery { Status = TaskStatus.Completed };
 
         _mockRepository
             .Setup(x => x.GetByStatusAsync(TaskStatus.Completed, It.IsAny<CancellationToken>()))
@@ -66,7 +63,7 @@ public class GetTasksByStatusQueryHandlerTests
         // Assert
         result.Should().NotBeNull();
         result.Should().HaveCount(2);
-        result.Should().OnlyContain(t => t.Status == "Completed");
+        result.Should().OnlyContain(t => t.Status == TaskStatus.Completed);
 
         _mockRepository.Verify(x => x.GetByStatusAsync(TaskStatus.Completed, It.IsAny<CancellationToken>()), Times.Once);
     }
@@ -86,7 +83,7 @@ public class GetTasksByStatusQueryHandlerTests
             }
         };
 
-        var query = new GetTasksByStatusQuery { Status = "inprogress" }; // lowercase
+        var query = new GetTasksByStatusQuery { Status = TaskStatus.InProgress };
 
         _mockRepository
             .Setup(x => x.GetByStatusAsync(TaskStatus.InProgress, It.IsAny<CancellationToken>()))
@@ -100,7 +97,7 @@ public class GetTasksByStatusQueryHandlerTests
         // Assert
         result.Should().NotBeNull();
         result.Should().HaveCount(1);
-        result.First().Status.Should().Be("InProgress");
+        result.First().Status.Should().Be(TaskStatus.InProgress);
     }
 
     [Fact]
@@ -115,45 +112,6 @@ public class GetTasksByStatusQueryHandlerTests
         await Assert.ThrowsAsync<InvalidTaskOperationException>(() => handler.Handle(query, CancellationToken.None));
 
         _mockRepository.Verify(x => x.GetByStatusAsync(It.IsAny<TaskStatus>(), It.IsAny<CancellationToken>()), Times.Never);
-    }
-
-    [Theory]
-    [InlineData("NotStarted")]
-    [InlineData("InProgress")]
-    [InlineData("Completed")]
-    [InlineData("notstarted")]
-    [InlineData("inprogress")]
-    [InlineData("completed")]
-    [InlineData("NOTSTARTED")]
-    [InlineData("INPROGRESS")]
-    [InlineData("COMPLETED")]
-    public async Task Handle_ValidStatusVariations_ShouldWork(string statusString)
-    {
-        // Arrange
-        var tasks = new List<ManagementTask>
-        {
-            new ManagementTask
-            {
-                Id = Guid.NewGuid(),
-                Title = "Test Task",
-                Status = TaskStatus.NotStarted,
-                CreatedAt = DateTime.UtcNow
-            }
-        };
-
-        var query = new GetTasksByStatusQuery { Status = statusString };
-
-        _mockRepository
-            .Setup(x => x.GetByStatusAsync(It.IsAny<TaskStatus>(), It.IsAny<CancellationToken>()))
-            .ReturnsAsync(tasks);
-
-        var handler = new GetTasksByStatusQueryHandler(_mockRepository.Object, _mapper);
-
-        // Act
-        var result = await handler.Handle(query, CancellationToken.None);
-
-        // Assert
-        result.Should().NotBeNull();
     }
 }
 
